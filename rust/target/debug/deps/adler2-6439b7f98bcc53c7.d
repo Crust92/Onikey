3d231e2862254090a5a2704d7a018e3f755c1df4
xtcrust <xtczone000000@gmail.com>
2026-08-10 01:13:28 +0700

    chore: gỡ rust/target lỡ track khỏi git
    .rustc_info.json được add trước khi có dòng ignore /rust/target/.

--- rust/target/debug/deps/adler2-6439b7f98bcc53c7.d
+++
@@ -1,8 +0,0 @@
-/home/d3v/Onikey/rust/target/debug/deps/adler2-6439b7f98bcc53c7.d: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
-
-/home/d3v/Onikey/rust/target/debug/deps/libadler2-6439b7f98bcc53c7.rlib: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
-
-/home/d3v/Onikey/rust/target/debug/deps/libadler2-6439b7f98bcc53c7.rmeta: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
-
-/home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs:
-/home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs:
--- rust/target/debug/deps/cfg_if-72a7719c5a503f05.d
+++
@@ -1,7 +0,0 @@
-/home/d3v/Onikey/rust/target/debug/deps/cfg_if-72a7719c5a503f05.d: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/d3v/Onikey/rust/target/debug/deps/libcfg_if-72a7719c5a503f05.rlib: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/d3v/Onikey/rust/target/debug/deps/libcfg_if-72a7719c5a503f05.rmeta: /home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/home/d3v/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs:
